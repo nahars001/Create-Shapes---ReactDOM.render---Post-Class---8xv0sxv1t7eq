df07526fac92201098f604ab561af678acd5529f
Nahar Singh <naharsingh151299@gmail.com>
2023-01-07 01:14:21 +0530

    submit

--- src/components/Creater.js
+++ src/components/Creater.js
@@ -1,7 +1,7 @@
 import React from "react";
 function Creater(props) {
    
-    console.log(props.data);
+
     const children = props.data.map((val) => (
         React.createElement("div", 
         { id: val["key"] , className: val["shape"]},
